37adaa421e4d1ccef4bd37bcd6e4bc32162b4677
Renan Lessa <renanlessa@gmail.com>
2014-11-20 17:05:57 -0200

    Novos exercicios, apostilas, exemplos

---
+++ Semana 02/src/com/flexxo/exercicios/Exec11.java
@@ -0,0 +1,16 @@
+package com.flexxo.exercicios;
+
+/*
+ * Escreva um programa que leia o nome de 5 pessoas e ao final
+ * imprima o nome destas pessoas na ordem inversa em que foram digitados
+ * 
+ */
+
+public class Exec11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Semana 02/src/com/flexxo/objetos/CarteiraDeEstudante.java
@@ -0,0 +1,14 @@
+package com.flexxo.objetos;
+
+class CarteiraDeEstudante {
+	
+	String nome;
+	int numero;
+	
+	public static void main(String[] args) {
+		CarteiraDeEstudante carteira = new CarteiraDeEstudante();
+		carteira.nome = "Fernando Gomes";
+		carteira.numero = 123123;
+		
+	}
+}
