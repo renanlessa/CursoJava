2c91e326f6bbee3cc66f041555001a0bee246e53
Renan Lessa <renanlessa@gmail.com>
2014-12-03 16:38:24 -0200

    adicionando novos exercicios

---
+++ Semana 03/src/com/flexxo/heranca/exercicios/TestaExec01.java
@@ -0,0 +1,43 @@
+package com.flexxo.heranca.exercicios;
+
+/*
+ * 1. 
+ * Continue escrevendo a classe EmpregadoDaFaculdade.
+ * 
+ * Seus membros devem ser:
+ * string nome
+ * double salario
+ * 
+ * metodo getGastos que retorna o salario
+ * metodo getInfo que retorna o nome e o salario
+ * escrever os metodos setters e getters
+ * 
+ * 
+ * os atributos devem ser privados e os metodos publicos
+ * 
+ * 
+ * 2.
+ * Escreva uma nova classe chamada Professor que extende de EmpregadoDaFaculdade.
+ * 
+ * Os gastos para professor muda, pois além do seu salario base, temos um acrescimo de 15 reais
+ * por hora/aula.
+ * 
+ * Crie um atributo do tipo inteiro chamados horasDeAula e reescreva os metodos
+ * da classe EmpregadoDaFaculdade, o getGastos deve devolver o salario mais o calculo das horas
+ * e o getInfo deve retornar o numero de hora aula
+ * 
+ * 
+ * 3. 
+ * Escreva uma nova classe chamada GeradorDeRelatorio
+ * a classe deve ter apenas um metodo void chamado adiciona que recebe um empregado
+ * e imprime o gasto e as infos
+ *
+ * 
+ */
+
+
+public class TestaExec01 {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ Semana 03/src/com/flexxo/heranca/exercicios/TestaExec02.java
@@ -0,0 +1,28 @@
+package com.flexxo.heranca.exercicios;
+
+/*
+ * Escreva uma classe abstrata chamada “CartaoWeb”. 
+ * Essa classe representa todos os tipos de cartões web e 
+ * conterá apenas um atributo: destinatario (tipo String). 
+ * 
+ * Nessa classe você deverá também declarar o método void showMessage(), que será abstrato. 
+ * 
+ * Crie classes filhas da classe CartaoWeb: DiaDosNamorados, Natal, Aniversario. 
+ * 
+ * Cada uma dessas classes deve conter um método construtor que receba o nome do destinatário 
+ * do cartão. 
+ * 
+ * Cada classe também deve implementar o método showMessage(), mostrando uma mensagem ao usuário 
+ * com seu nome e que seja específica para a data de comemorativa do cartão.
+ * Escreva um programa e no método main crie um array de CartaoWeb. 
+ * Insira instâncias dos 3 tipos de cartões neste array. 
+ * 
+ * Após, use um laço for para exibir as mensagens deste cartão chamando o método showMessage(). 
+ * 
+ */
+
+public class TestaExec02 {
+	public static void main(String[] args) {
+
+	}
+}
